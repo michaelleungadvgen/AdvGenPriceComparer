483b6300bc0f58c0f93573dcdf83849686f3d175
michaelleungadvgen <michael.leung@advgen.com>
2024-11-23 08:57:05 +1000

    Added Item

--- AdvGenPriceComparer.Core/Models/Item.cs
+++ AdvGenPriceComparer.Core/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,48 @@ namespace AdvGenPriceComparer.Core.Models
 {
     public class Item
     {
-        
-        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+        public string Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+        public string Store { get; set; }
+        public string StoreUrl { get; set; }
+        public string StoreLogoUrl { get; set; }
+        public string StoreLocation { get; set; }
+        public string StorePhoneNumber { get; set; }
+        public string StoreEmail { get; set; }
+        public string StoreAddress { get; set; }
+        public string StoreCity { get; set; }
+        public string StoreState { get; set; }
+        public string StoreZip { get; set; }
+        public string StoreCountry { get; set; }
+        public string StoreLatitude { get; set; }
+        public string StoreLongitude { get; set; }
+        public string StoreHours { get; set; }
+        public string StoreHoursNote { get; set; }
+        public string StoreHoursNote2 { get; set; }
+        public string StoreHoursNote3 { get; set; }
+        public string StoreHoursNote4 { get; set; }
+        public string StoreHoursNote5 { get; set; }
+        public string StoreHoursNote6 { get; set; }
+        public string StoreHoursNote7 { get; set; }
+        public string StoreHoursNote8 { get; set; }
+        public string StoreHoursNote9 { get; set; }
+        public string StoreHoursNote10 { get; set; }
+        public string StoreHoursNote11 { get; set; }
+        public string StoreHoursNote12 { get; set; }
+        public string StoreHoursNote13 { get; set; }
+        public string StoreHoursNote14 { get; set; }
+        public string StoreHoursNote15 { get; set; }
+        public string StoreHoursNote16 { get; set; }
+        public string StoreHoursNote17 { get; set; }
+        public string StoreHoursNote18 { get; set; }
+        public string StoreHoursNote19 { get; set; }
+        public string StoreHoursNote20 { get; set; }
+       public Dictionary<string, string> ExtraInformation { set; get; }
     }
 }
